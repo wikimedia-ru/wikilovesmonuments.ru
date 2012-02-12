4aeb55c993de71ee7977174ea1041ce0073ed7a1
Sergey Leschina <i.am.putnik@gmail.com>
2012-02-12 16:08:14 +0400

    Better styles for templates

--- house/views.py
+++ house/views.py
@@ -2,13 +2,13 @@ from django.utils.translation import ugettext_lazy as _
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.conf import settings
+from django.db.models import Count
 
 from house.models import Street, House
 
 
-
 def street_list(request):
-    s_list = Street.objects.order_by('name')
+    s_list = Street.objects.order_by('name').annotate(house_count=Count('house'))
 
     return render_to_response('house/street_list.html', {
         'list': s_list,
