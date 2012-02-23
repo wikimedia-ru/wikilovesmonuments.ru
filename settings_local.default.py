f8cf4f991cb9761eca8c7d2f891d1ea430a2c321
Sergey Leschina <i.am.putnik@gmail.com>
2012-02-23 15:05:05 +0400

    Add example file for local settings

---
+++ settings_local.default.py
@@ -0,0 +1,25 @@
+# Local settings for vologda project.
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+    #('Name', 'mail@example.com'),
+)
+
+MANAGERS = ADMINS
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+SECRET_KEY = ''
+CMADE_KEY = ''
+
