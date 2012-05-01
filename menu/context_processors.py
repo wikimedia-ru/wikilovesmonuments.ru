6ccc9befac58d2cc02d5e371909c492fb903c0f8
putnik <putnik@putnik-laptop.(none)>
2012-05-01 04:06:28 +0400

    Active menu item for subpages

--- menu/context_processors.py
+++ menu/context_processors.py
@@ -2,4 +2,8 @@ from menu.models import MenuItem
 
 
 def menu_items(request):
-    return { 'menu': MenuItem.objects.all() }
+    items = MenuItem.objects.all()
+    for item in items:
+        if (request.path[:len(item.url)] == item.url):
+            item.active = True
+    return { 'menu': items }
