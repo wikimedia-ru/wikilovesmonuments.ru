b1c0bffe4002aae1b85fd7c58dd67f3263a4babf
putnik <putnik@putnik-laptop.(none)>
2012-05-01 04:42:40 +0400

    Add map with all houses

--- house/views.py
+++ house/views.py
@@ -8,9 +8,21 @@ from house.models import *
 
 def index_page(request):
     s_list = Street.objects.order_by('name').annotate(house_count=Count('house'))
+    h_list = House.objects.all()
+    map_border = h_list.aggregate(Max('coord_lon'), Min('coord_lon'), Max('coord_lat'), Min('coord_lat'))
+    if map_border['coord_lat__max'] != None:
+        map_center = {
+            'lat': (map_border['coord_lat__max'] + map_border['coord_lat__min']) / 2,
+            'lon': (map_border['coord_lon__max'] + map_border['coord_lon__min']) / 2,
+        }
+    else:
+        map_center = False
 
     return render_to_response('house/index.html', {
         'street_list': s_list,
+        'house_list': h_list,
+        'map_center': map_center,
+        'CMADE_KEY': settings.CMADE_KEY,
         }, context_instance=RequestContext(request))
 
 
