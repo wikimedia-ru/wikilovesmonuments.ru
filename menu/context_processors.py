64de0c6f1ba5d55fa711849e2aaa23bde255453d
Sergey Leschina <mail@putnik.ws>
2012-05-09 07:42:44 +0400

    Fix menu's context processor

--- menu/context_processors.py
+++ menu/context_processors.py
@@ -4,6 +4,9 @@ from menu.models import MenuItem
 def menu_items(request):
     items = MenuItem.objects.order_by('order')
     for item in items:
-        if (request.path[:len(item.url)] == item.url):
+        if item.url == '/':
+            if request.path == '/':
+                item.active = True
+        elif request.path[:len(item.url)] == item.url:
             item.active = True
     return { 'menu': items }
