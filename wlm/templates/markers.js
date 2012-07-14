a2657dc1900868a31f31a314b7f456f432dc76dc
sarutobi <ilychev@gmail.com>
2012-07-14 20:50:07 +0400

    Loading map markers by chunks

--- wlm/templates/markers.js
+++ wlm/templates/markers.js
@@ -1,7 +1,7 @@
-{%spaceless%}
+
 { "markers":[
 {%for m in monuments%}
 {"id":{{m.id}},"lat":{{m.coord_lat|stringformat:'f'}},"lon":{{m.coord_lon|stringformat:'f'}},"name":"{{m.name|default:"Неизвестно"|truncatewords:5}}"}{%if forloop.last%}{%else%},{%endif%}
 {%endfor%}
 ]}
-{%endspaceless%}
+
