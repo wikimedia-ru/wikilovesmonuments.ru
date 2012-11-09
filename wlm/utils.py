2b85a6501ac1155e732658dd98e3690b2c0dfebb
Sergey Leschina <mail@putnik.ws>
2012-11-09 19:15:15 +0300

    Fix GeoIP for foreign countries

--- wlm/utils.py
+++ wlm/utils.py
@@ -1,19 +1,21 @@
-# -*- coding: utf-8 -*-
-
-
 from django.contrib.gis.geoip import GeoIP
 import settings
 
 
 def get_region(ip):
+    default_code = '47' # Moscow
     g = GeoIP()
     try:
-        code = g.city(ip)['region']
+        city = g.city(ip)
+        if city['country_code'] == 'RU':
+            code = city['region']
+        else:
+            code = default_code
     except:
-        code = "47"
-    if code == "48": 
-        code = "47"  #Moscow
-    elif code == "66":
-        code = "42"  #Spb
+        code = default_code
+    if code == '48': 
+        code = '47'  # Moscow
+    elif code == '66':
+        code = '42'  # Spb
 
     return code
