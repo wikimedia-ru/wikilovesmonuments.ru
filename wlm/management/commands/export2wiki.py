4a0bc76361053cb2998c50acc61e7bcfea4a75f4
Sergey Leschina <mail@putnik.ws>
2013-08-21 03:24:32 +0400

    Fix export into Wikipedia

--- wlm/management/commands/export2wiki.py
+++ wlm/management/commands/export2wiki.py
@@ -1,5 +1,5 @@
 # -*- encoding: utf-8 -*-
-import urllib
+import urllib, urllib2
 import json
 import re
 
@@ -9,13 +9,17 @@ from django.core.management.base import BaseCommand, CommandError
 from django.core.exceptions import ObjectDoesNotExist
 
 from wlm.models import Region, Monument, MonumentPhoto
+from settings import WIKI_NAME, WIKI_PASSWORD
 
 
 class Command(BaseCommand):
     help = u'Export cultural heritage into Wikipedia'
+    cookie = ''
+
 
     def handle(self, *args, **options):
         regions = Region.objects.all()
+        self.login()
         for region in regions:
             print '%s (%s): ' % (region.name, region.id)
             monuments = Monument.objects.filter(region_id=region.id)
@@ -28,14 +32,14 @@ class Command(BaseCommand):
 
 
     def update_page(self, title, monuments):
-        if not len(monuments):
+        if not len(monuments) or len(monuments) > 500:
             return False
         text = u'{{WLM/заголовок}}\n'
         for m in monuments:
             text += u'{{WLM/строка\n'
             text += u'| id = %s\n' % m.kult_id
             text += u'| название = %s\n' % m.name
-            text += u'| нп = %s\n' % m.city
+            text += u'| нп = %s\n' % m.city.name
             text += u'| адрес = %s\n' % m.address
             text += u'| регион = %s\n' % m.region
             text += u'| lat = %s\n' % m.coord_lat
@@ -66,6 +70,31 @@ class Command(BaseCommand):
             'token': token,
         }
         answer = self.api_request(api_params, True)
+        print answer
+        
+        return True
+
+
+    def login(self):
+        api_params = {
+            'action': 'login',
+            'lgname': WIKI_NAME.encode('utf8'),
+            'lgpassword': WIKI_PASSWORD.encode('utf8'),
+        }
+        answer = self.api_request(api_params, True)
+
+        token = answer['login']['token']
+        api_params['lgtoken'] = token
+
+        sessionid = answer['login']['sessionid']
+        self.cookie = 'ruwikiSession=%s' % sessionid
+
+        answer = self.api_request(api_params, True)
+        prefix = answer['login']['cookieprefix']
+        cookie = '%sUserID=%s' % (prefix, answer['login']['lguserid'])
+        cookie += '; %sUserName=%s' % (prefix, answer['login']['lgusername'])
+        cookie += '; %sSession=%s' % (prefix, answer['login']['sessionid'])
+        self.cookie = cookie
         
         return True
 
@@ -78,10 +107,16 @@ class Command(BaseCommand):
         get_string = urllib.urlencode(params)
 
         server = 'http://ru.wikipedia.org'
+
         if post:
-            f = urllib.urlopen('%s/w/api.php' % server, get_string)
+            req = urllib2.Request(url='%s/w/api.php' % server, data=get_string)
         else:
-            f = urllib.urlopen('%s/w/api.php?%s' % (server, get_string))
+            req = urllib2.Request(url='%s/w/api.php?%s' % (server, get_string))
+
+        if self.cookie:
+            req.add_header('Cookie', self.cookie)
+
+        f = urllib2.urlopen(req)
 
         return json.load(f)
 
